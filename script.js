fc6f01d54c2599ebf599a3e112985c5f889ba65e
Bani_Redux <61953633+jordyn-haessler@users.noreply.github.com>
2022-11-13 13:20:49 -0600

    v.0.1
    initial beta release

---
+++ script.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab =>{
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.tabTarget)
+		tabContents.forEach(tabContent => tabContent.classList.remove('active'))
+		tabs.forEach(tab => tab.classList.remove('active'))
+		tabs.forEach(tab => tab.classList.add('nonactive'))
+		target.classList.add('active')
+		tab.classList.add("active")
+		tab.classList.remove("nonactive")
+	})
+ })// JavaScript Document
